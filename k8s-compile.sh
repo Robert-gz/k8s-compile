fb4502e252aaa4b5c8b4b55fa2a9f1693290b1f4
Robert-gz <30598465+Robert-gz@users.noreply.github.com>
2017-09-24 22:50:26 +0800

    Update k8s-compile.sh

--- k8s-compile.sh
+++ k8s-compile.sh
@@ -1,3 +1,4 @@
+set -x 
 export K8S_VERSION=1.7.6
 nohup dockerd -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock 2>1 &
 wget https://github.com/kubernetes/kubernetes/archive/v${K8S_VERSION}.tar.gz
